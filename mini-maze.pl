2c8d1443a101247256f40644d766441292967460
yakovb <yakov.boglev@gmail.com>
2014-11-08 15:00:35 +0000

    Path is found (correctly) but it doesn't print??

--- mini-maze.pl
+++ mini-maze.pl
@@ -1,20 +1,26 @@
 mazeSize(2, 2).
 
-move([OldX, Y] , [NewX, Y], [[NewX, Y]|_]) :-
-	NewX is OldX + 1 ,
+move([X,Y], To, Path) :-
+	NewX is X + 1 ,
 	mazeSize(XBound, _) ,
-	NewX =< XBound.
+	NewX =< XBound ,
+	go([NewX,Y], To, [[NewX,Y]|Path]).
 
-move([X, OldY] , [X, NewY], [[X, NewY]|_]) :-
-	NewY is OldY + 1 ,
+move([X,Y], To, Path) :-
+	NewY is Y + 1 ,
 	mazeSize(_, YBound) ,
-	NewY =< YBound.
+	NewY =< YBound ,
+	go([X,NewY], To, [[X,NewY]|Path]).
 
 
-isFinished(To, To, _).
+isFinished(To, To).
 
 
-solve(From, To, Path) :-
-	move(From, Step, Path) ,
-	move(Step, To, Path) ,
-	isFinished(Step, To, Path).
+go(From, To, _) :-
+	isFinished(From, To).
+
+go(From, To, Path) :-
+	move(From, To, Path).
+
+
+solve(From, To, [From|Rest]) :- go(From, To, [From|[]]).
