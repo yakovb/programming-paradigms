75ed655c213ea202b9e4512a96d9cd811493fa6b
yakovb <yakov.boglev@gmail.com>
2014-11-11 15:00:37 +0000

    Added more line building actions, but infinite loop! Add elems to line if coordinate is barrier, in the path, not in the path. Is looping forever somewhere and overflowing the stack.

--- print-maze.pl
+++ print-maze.pl
@@ -1,28 +1,53 @@
+module(print-maze, [printMaze/1]).
 use_module(maze).
 
 
+buildLine(Row, _, _, _) :-
+	mazeSize(_, Ylim) ,
+	Row > Ylim + 1.
 
-buildLine(Row, Column, Acc, Line) :-
+buildLine(Row, Column, reverse(Line), Path) :-
+	mazeSize(Xlim, _) ,
+	Column > Xlim + 1 ,
+	format(Line) ,
+	NewRow is Row + 1 ,
+	buildLine(NewRow, 0, [], Path).
+
+
+buildLine(Row, Column, Line, Path) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
 	NewCol is Column + 1 ,
-	buildLine(Row, NewCol, [+|Acc], Line).
+	buildLine(Row, NewCol, [+|Line], Path).
 
-buildLine(Row, Column, Acc, Line) :-
+buildLine(Row, Column, Line, Path) :-
 	mazeSize(_, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
 	NewCol is Column + 1 ,
-	buildLine(Row, NewCol, [-|Acc], Line).
+	buildLine(Row, NewCol, [-|Line], Path).
 
-buildLine(Row, Column, Acc, Line) :-
+buildLine(Row, Column, Line, Path) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
 	NewCol is Column + 1 ,
-	buildLine(Row, NewCol, ["|"|Acc], Line).
+	buildLine(Row, NewCol, ["|"|Line], Path).
+
+buildLine(Row, Column, Line, Path) :-
+	barrier(Row, Column) ,
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, ["X"|Line], Path).
+
+buildLine(Row, Column, Line, Path) :-
+	member([Row, Column], Path) ,
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, [*|Line], Path).
+
+buildLine(Row, Column, Line, Path) :-
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, [.|Line], Path).
 
 
 printMaze(Path) :-
-	buildLine(0, 0, [], Line) ,
-	format(Line).
+	buildLine(0, 0, [], Path).
