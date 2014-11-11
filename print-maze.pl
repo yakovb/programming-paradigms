e0c2b870b8f2902bbd189a0a82ad7beabf59be8e
yakovb <yakov.boglev@gmail.com>
2014-11-11 13:34:46 +0000

    Amended boundary printing rules. They now call each other recursively. Still have to add base case.

--- print-maze.pl
+++ print-maze.pl
@@ -7,19 +7,28 @@ inRange(X,Y) :-
 	Y >= 0 ,
 	Y =< (Ylim + 1).
 
-buildLine(Row, Column, [+|_], _) :-
+buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
-	(Column =:= 0  ;  Column =:= (Xlim + 1)).
+	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
+	NewRow is Row + 1 ,
+	NewCol is Column + 1 ,
+	buildLine(NewRow, NewCol, [+|Acc], Line).
 
-buildLine(Row, _, [-|_], _) :-
+buildLine(Row, Column, Acc, Line) :-
 	mazeSize(_, Ylim) ,
-	(Row =:= 0  ;  Row =:= (Ylim + 1)).
+	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
+	NewRow is Row + 1 ,
+	NewCol is Column + 1 ,
+	buildLine(NewRow, NewCol, [-|Acc], Line).
 
-buildLine(Row, Column, ["|"|_], _) :-
+buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
-	(Column =:= 0  ;  Column =:= (Xlim + 1)).
+	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
+	NewRow is Row + 1 ,
+	NewCol is Column + 1 ,
+	buildLine(NewRow, NewCol, ["|"|Acc], Line).
 
 
 printLine(Row, Column) :-
