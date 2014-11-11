3fd28b7aea367970f54c00b976de655901081274
yakovb <yakov.boglev@gmail.com>
2014-11-11 13:41:21 +0000

    Line building starts at mazesize + 1 then works downwards

--- print-maze.pl
+++ print-maze.pl
@@ -1,36 +1,35 @@
 use_module(maze).
 
-inRange(X,Y) :-
-	mazeSize(Xlim, Ylim) ,
-	X >= 0 ,
-	X =< (Xlim + 1) ,
-	Y >= 0 ,
-	Y =< (Ylim + 1).
+
+
 
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
-	NewRow is Row + 1 ,
-	NewCol is Column + 1 ,
+	NewRow is Row - 1 ,
+	NewCol is Column - 1 ,
 	buildLine(NewRow, NewCol, [+|Acc], Line).
 
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(_, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
-	NewRow is Row + 1 ,
-	NewCol is Column + 1 ,
+	NewRow is Row - 1 ,
+	NewCol is Column - 1 ,
 	buildLine(NewRow, NewCol, [-|Acc], Line).
 
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
-	NewRow is Row + 1 ,
-	NewCol is Column + 1 ,
+	NewRow is Row - 1 ,
+	NewCol is Column - 1 ,
 	buildLine(NewRow, NewCol, ["|"|Acc], Line).
 
 
-printLine(Row, Column) :-
-	buildLine(Row, Column, [], Line) ,
+printMaze(Path) :-
+	mazeSize(Xlim, Ylim) ,
+	RowCountDown is Xlim + 1 ,
+	ColCountDown is Ylim + 1 ,
+	buildLine(RowCountDown, ColCountDown, [], Line) ,
 	format(Line).
