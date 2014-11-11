73b149c9dc74c0a75105b9360d22b94892901a20
yakovb <yakov.boglev@gmail.com>
2014-11-11 15:55:02 +0000

    Corrected maze printing re: height/width Still need to correct actual path calculation Still need to add height and length axes

--- print-maze.pl
+++ print-maze.pl
@@ -3,14 +3,14 @@ use_module(maze).
 
 %stop if row outside boundary
 buildLine(Row, _, _, _) :-
-	mazeSize(_, Ylim) ,
-	Row > Ylim + 1 .
+	mazeSize(Height, _) ,
+	Row > Height + 1 .
 
 %move to next row since column is outside boundary
 %print the line built so far
 buildLine(Row, Column, Line, Path) :-
-	mazeSize(Xlim, _) ,
-	Column > Xlim + 1 ,
+	mazeSize(_, Length) ,
+	Column > Length + 1 ,
 	reverse(Line, RLine) ,
 	format(RLine) ,
 	format('~n') ,
@@ -19,36 +19,36 @@ buildLine(Row, Column, Line, Path) :-
 
 %coordinate is a corner
 buildLine(Row, Column, Line, Path) :-
-	mazeSize(Xlim, Ylim) ,
-	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
-	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
+	mazeSize(Height, Length) ,
+	(Row =:= 0  ;  Row =:= (Height + 1)) ,
+	(Column =:= 0  ;  Column =:= (Length + 1)) ,
 	NewCol is Column + 1 ,
 	buildLine(Row, NewCol, ['+'|Line], Path).
 
 %coordinate is a top/bottom border
 buildLine(Row, Column, Line, Path) :-
-	mazeSize(_, Ylim) ,
-	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
+	mazeSize(Height, _) ,
+	(Row =:= 0  ;  Row =:= (Height + 1)) ,
 	NewCol is Column + 1 ,
 	buildLine(Row, NewCol, ['-'|Line], Path).
 
 %coordinate is a side border
 buildLine(Row, Column, Line, Path) :-
-	mazeSize(Xlim, Ylim) ,
-	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
-	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
+	mazeSize(Height, Length) ,
+	(Row =\= 0  ;  Row =\= (Height + 1)) ,
+	(Column =:= 0  ;  Column =:= (Length + 1)) ,
 	NewCol is Column + 1 ,
 	buildLine(Row, NewCol, ['|'|Line], Path).
 
 %coordinate is a barrier
 buildLine(Row, Column, Line, Path) :-
-	barrier(Row, Column) ,
+	barrier(Column, Row) ,
 	NewCol is Column + 1 ,
 	buildLine(Row, NewCol, ['x'|Line], Path).
 
 %coordinate is in the path
 buildLine(Row, Column, Line, Path) :-
-	member([Row, Column], Path) ,
+	member([Column, Row], Path) ,
 	NewCol is Column + 1 ,
 	buildLine(Row, NewCol, ['*'|Line], Path).
 
