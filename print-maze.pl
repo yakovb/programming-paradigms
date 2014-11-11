ec641dc00a4fe65b6a4a6656e4984859bb15915d
yakovb <yakov.boglev@gmail.com>
2014-11-11 13:45:06 +0000

    Can't build a line backwards! Printing has to start from the first line of the maze down otherwise it doesn't work. Also removed the row increment - this has to happen after a column is completely built.

--- print-maze.pl
+++ print-maze.pl
@@ -2,34 +2,27 @@ use_module(maze).
 
 
 
-
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
-	NewRow is Row - 1 ,
-	NewCol is Column - 1 ,
-	buildLine(NewRow, NewCol, [+|Acc], Line).
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, [+|Acc], Line).
 
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(_, Ylim) ,
 	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
-	NewRow is Row - 1 ,
-	NewCol is Column - 1 ,
-	buildLine(NewRow, NewCol, [-|Acc], Line).
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, [-|Acc], Line).
 
 buildLine(Row, Column, Acc, Line) :-
 	mazeSize(Xlim, Ylim) ,
 	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
 	(Column =:= 0  ;  Column =:= (Xlim + 1)) ,
-	NewRow is Row - 1 ,
-	NewCol is Column - 1 ,
-	buildLine(NewRow, NewCol, ["|"|Acc], Line).
+	NewCol is Column + 1 ,
+	buildLine(Row, NewCol, ["|"|Acc], Line).
 
 
 printMaze(Path) :-
-	mazeSize(Xlim, Ylim) ,
-	RowCountDown is Xlim + 1 ,
-	ColCountDown is Ylim + 1 ,
-	buildLine(RowCountDown, ColCountDown, [], Line) ,
+	buildLine(0, 0, [], Line) ,
 	format(Line).
