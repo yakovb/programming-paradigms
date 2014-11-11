da21456099f769c809fbd1364a3171409f440090
yakovb <yakov.boglev@gmail.com>
2014-11-11 13:25:21 +0000

    Added rules to build maze boundary

--- print-maze.pl
+++ print-maze.pl
@@ -1,12 +1,27 @@
 use_module(maze).
 
-inRange([X,Y]) :-
+inRange(X,Y) :-
 	mazeSize(Xlim, Ylim) ,
 	X >= 0 ,
 	X =< (Xlim + 1) ,
 	Y >= 0 ,
 	Y =< (Ylim + 1).
 
-printLine(Row, Line) :-
-	inRange(Row) ,
+buildLine(Row, Column, [+|_], _) :-
+	mazeSize(Xlim, Ylim) ,
+	(Row =:= 0  ;  Row =:= (Ylim + 1)) ,
+	(Column =:= 0  ;  Column =:= (Xlim + 1)).
+
+buildLine(Row, _, [-|_], _) :-
+	mazeSize(_, Ylim) ,
+	(Row =:= 0  ;  Row =:= (Ylim + 1)).
+
+buildLine(Row, Column, ["|"|_], _) :-
+	mazeSize(Xlim, Ylim) ,
+	(Row =\= 0  ;  Row =\= (Ylim + 1)) ,
+	(Column =:= 0  ;  Column =:= (Xlim + 1)).
+
+
+printLine(Row, Column) :-
+	buildLine(Row, Column, [], Line) ,
 	format(Line).
