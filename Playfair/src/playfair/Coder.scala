30311ef0ffdeba38a8223cd858195da5493bb1a3
byakov <yakov.boglev@gmail.com>
2015-01-12 19:55:13 +0000

    Added Code class skeleton

---
+++ Playfair/src/playfair/Coder.scala
@@ -0,0 +1,10 @@
+package playfair
+
+/**
+ * Created by Yakov Boglev on 12/01/2015.
+ */
+class Coder(keyword: String) {
+  def encode(plaintText: String): String = ???
+
+  def decode(secretText: String): String = ???
+}
