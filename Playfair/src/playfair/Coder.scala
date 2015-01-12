116c0c18cef10d6bd84394b0dae7b8ea61186fb5
byakov <yakov.boglev@gmail.com>
2015-01-12 20:03:19 +0000

    Created further outline of functions for Coder class

--- Playfair/src/playfair/Coder.scala
+++ Playfair/src/playfair/Coder.scala
@@ -4,7 +4,12 @@ package playfair
  * Created by Yakov Boglev on 12/01/2015.
  */
 class Coder(keyword: String) {
+  val alphabet = "abcdefghiklmnopqrstuvwxyz".toList
+  val rowBounds = Array(0, 5, 10, 15, 20)
+
   def encode(plaintText: String): String = ???
 
   def decode(secretText: String): String = ???
+
+  def createCodeBlock(): Array[String] = ???
 }
