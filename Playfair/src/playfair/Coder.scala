cda790baf3645ed0e66921987e8f6a7e1aec0546
byakov <yakov.boglev@gmail.com>
2015-01-13 11:01:21 +0000

    Completed functions for changing letters Also moved processLetters out of encode. This is generic enough to handle encode and decode requests, so it now does so depending on whether the called specifies encode or decode.

--- Playfair/src/playfair/Coder.scala
+++ Playfair/src/playfair/Coder.scala
@@ -8,49 +8,6 @@ class Coder(keyword: String) {
   val rowBounds = Array(0, 5, 10, 15, 20, 25)
   val codeBlock = createCodeBlock()
 
-  def encode(plaintText: String): String = {
-    def encodeHelper(lst: List[Char], acc: List[Char]): List[Char] = lst match {
-      case Nil                            => acc.reverse
-      case a::Nil                         => encodeHelper(Nil, processLetters(a, 'z') ::: acc)
-      case a::b::rest if a==b && a!='x'   => encodeHelper(b::rest, processLetters(a, 'x') ::: acc)
-      case a::b::rest if a==b && a=='x'   => encodeHelper(b::rest, processLetters(a, 'q') ::: acc)
-      case a::b::rest if a!=b             => encodeHelper(rest, processLetters(a, b) ::: acc)
-    }
-    def processLetters(x: Char, y: Char, direction: String): List[Char] = {
-      def sameRow(i: Int, j: Int): Boolean = {
-        val big = math.max(i, j)
-        val small = math.min(i, j)
-        rowBounds.indexWhere(p =>  p <= big && p + 1 > big) == rowBounds.indexWhere(p =>  p <= small && p + 1 > small)
-      }
-      def sameColumn(i: Int, j: Int): Boolean = {
-        i%5 == j%5
-      }
-      def rowOperation(k: Int): Int = direction match {
-        case "encode" => if (k + 1%5 == 0) k - 4 else k + 1
-        case "decode" =>
-          val indexMod = k % 5
-          if (indexMod - 1 < 0) k + 4 else indexMod - 1
-      }
-      def columnOperation(k: Int): Int = direction match {
-        case "encode" => if (k >= 20) k - 20 else k + 1
-        case "decode" => if (k <= 4) k + 20 else k - 1
-      }
-      def rectangleOperation(i: Int, j: Int): List[Char] = {
-        val iDiff = 4 - i%5
-        val jDiff = 4 - j%5
-        val absDiff = math.abs(iDiff - jDiff)
-        if (iDiff > jDiff) List(codeBlock(i - absDiff), codeBlock(j + absDiff))
-        else List(codeBlock(i + absDiff), codeBlock(j - absDiff))
-      }
-      val i = codeBlock indexOf x
-      val j = codeBlock indexOf y
-
-      // requires reverse
-    }
-  }
-
-  def decode(secretText: String): String = ???
-
   def createCodeBlock(): Array[Char] = {
     def blockHelper(lst: List[Char]): List[Char] = lst match {
       case Nil                  => lst
@@ -61,4 +18,57 @@ class Coder(keyword: String) {
     val alphabetPart = (for (c <- alphabet if !keyPart.contains(c)) yield c).toArray
     keyPart ++ alphabetPart
   }
+
+  def encode(plaintText: String): String = {
+    def encodeHelper(lst: List[Char], acc: List[Char]): List[Char] = {
+      val encode = "encode"
+      lst match {
+        case Nil => acc.reverse
+        case a :: Nil => encodeHelper(Nil, processLetters(a, 'z', encode) ::: acc)
+        case a :: b :: rest if a == b && a != 'x' => encodeHelper(b :: rest, processLetters(a, 'x', encode) ::: acc)
+        case a :: b :: rest if a == b && a == 'x' => encodeHelper(b :: rest, processLetters(a, 'q', encode) ::: acc)
+        case a :: b :: rest if a != b => encodeHelper(rest, processLetters(a, b, encode) ::: acc)
+      }
+    }
+    encodeHelper(plaintText.toList, List[Char]()).mkString
+  }
+
+  def decode(secretText: String): String = ???
+
+
+  def processLetters(x: Char, y: Char, direction: String): List[Char] = {
+    def sameRow(i: Int, j: Int): Boolean = {
+      val big = math.max(i, j)
+      val small = math.min(i, j)
+      rowBounds.indexWhere(p =>  p <= big && p + 1 > big) == rowBounds.indexWhere(p =>  p <= small && p + 1 > small)
+    }
+    def sameColumn(i: Int, j: Int): Boolean = {
+      i%5 == j%5
+    }
+    def rowOperation(k: Int): Int = direction match {
+      case "encode" => if (k + 1%5 == 0) k - 4 else k + 1
+      case "decode" =>
+        val indexMod = k % 5
+        if (indexMod - 1 < 0) k + 4 else indexMod - 1
+    }
+    def columnOperation(k: Int): Int = direction match {
+      case "encode" => if (k >= 20) k - 20 else k + 1
+      case "decode" => if (k <= 4) k + 20 else k - 1
+    }
+    def rectangleOperation(i: Int, j: Int): List[Char] = {
+      val iDiff = 4 - i%5
+      val jDiff = 4 - j%5
+      val absDiff = math.abs(iDiff - jDiff)
+      if (iDiff > jDiff) List(codeBlock(i - absDiff), codeBlock(j + absDiff))
+      else List(codeBlock(i + absDiff), codeBlock(j - absDiff))
+    }
+    val i = codeBlock indexOf x
+    val j = codeBlock indexOf y
+
+    // return new letters in reverse order as the output string is built like a stack
+    if (sameRow(i, j)) List(codeBlock(rowOperation(j)), codeBlock(rowOperation(i)))
+    else if (sameColumn(i, j)) List(codeBlock(columnOperation(j)), codeBlock(columnOperation(i)))
+    else rectangleOperation(i, j).reverse
+  }
+
 }
