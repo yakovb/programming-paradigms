3fd60618bf29a58aca3bee340d241152146c1bf4
yakovb <yakov.boglev@gmail.com>
2015-02-22 18:26:36 +0000

    Added first tests re: cell construction

--- sudoku-test.rkt
+++ sudoku-test.rkt
@@ -6,13 +6,15 @@
 (define sudoku-tests
   (test-suite
    "Tests for cell struct"
-   
-   (test-case
-    "basic cell struct contsruction"
-    (fail "default fail easy struct"))
-   
-   (test-case
-    "cell struct construction with default parameter"
-    (fail "default fail fancy struct"))))
-
+   (let ([c1 (cell (set 1 2 3) 1 1 'upper-left #t)]
+         [c2 (make-cell (set 3 4 5) 2 2 'lower-right)])
+     
+     (test-case
+      "basic cell struct contsruction"
+      (check-equal? (set 1 2 3) (cell-data c1)))
+     
+     (test-case
+      "cell struct construction with default parameter"
+      (fail "default fail fancy struct")))))
+  
 (run-tests sudoku-tests)
