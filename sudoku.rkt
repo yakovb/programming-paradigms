d4c078ac92a6f472de8ffdf914e241c27c514d0b
yakovb <yakov.boglev@gmail.com>
2015-02-22 19:36:51 +0000

    Function to make a set of possible cell values

--- sudoku.rkt
+++ sudoku.rkt
@@ -6,12 +6,26 @@
 (define (transform matrix)
   1)
 
+
+;; CONTRACT: make-set-of-possible-values: number -> set
+;;
+;; PURPOSE: make a set of possible values for a cell. If the
+;; passed in number is zero, possibilities are 1..9. Otherwise
+;; the only possibility is the number that was passed in. 
+;;
+(define (make-set-of-possible-values elem) 
+  (if (zero? elem) 
+      (set 1 2 3 4 5 6 7 8 9)
+      (set elem)))
+
+
 ;; CONTRACT: cell: set number number symbol boolean -> struct
 ;;
 ;; PURPOSE: construct a sudoku cell that contains a list of the cell's possible 
 ;; values, its row and column number, and the box it belongs to
 ;;
 (struct cell (data row col box singleton-checked?))
+
 
 ;; CONTRACT: make-cell: set number number symbol -> struct
 ;;
