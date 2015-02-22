981a8ea12f45366e2bdd17d15c14d118b03832e1
yakovb <yakov.boglev@gmail.com>
2015-02-22 18:41:59 +0000

    Added cell struct and helper constructor.

--- sudoku.rkt
+++ sudoku.rkt
@@ -5,3 +5,18 @@
 
 (define (transform matrix)
   1)
+
+;; CONTRACT: cell: set number number symbol boolean -> struct
+;;
+;; PURPOSE: construct a sudoku cell that contains a list of the cell's possible 
+;; values, its row and column number, and the box it belongs to
+;;
+(struct cell (data row col box singleton-checked?))
+
+;; CONTRACT: make-cell: set number number symbol -> struct
+;;
+;; PURPOSE: construct a sudoku cell as per default constructor
+;; except this version passes a default #f for the singleton-checked? value
+;;
+(define (make-cell data row col box [singleton-checked? #f])
+  (cell data row col box singleton-checked?))
