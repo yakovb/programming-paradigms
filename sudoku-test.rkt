766f0e61f5f3f9b810fa002f847e21b91a32e211
yakovb <yakov.boglev@gmail.com>
2015-03-01 12:05:46 +0000

    added puzzle to tests

--- sudoku-test.rkt
+++ sudoku-test.rkt
@@ -3,6 +3,17 @@
          rackunit
          rackunit/text-ui)
 
+(define puzzle '((0 2 5 0 0 1 0 0 0)
+                (1 0 4 2 5 0 0 0 0)
+                (0 0 6 0 0 4 2 1 0)
+                (0 5 0 0 0 0 3 2 0)
+                (6 0 0 0 2 0 0 0 9)
+                (0 8 7 0 0 0 0 6 0)
+                (0 9 1 5 0 0 6 0 0)
+                (0 0 0 0 7 8 1 0 3)
+                (0 0 0 6 0 0 5 9 0)
+                ))
+
 (define sudoku-tests
   (test-suite
    "Wrapper suite for all tests"
