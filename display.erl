112dc53fc2a106dc223bbca51a655c7154a6a9b3
yakovb <yakov.boglev@gmail.com>
2015-04-19 16:05:44 +0100

    display receives successful tuples and prints output

--- display.erl
+++ display.erl
@@ -6,6 +6,12 @@
 
 loop() ->
 	receive
+		{celsiusOK, Ctemp, Ftemp} ->
+			io:format("~.2f C is equivalent to ~.2f F~n", [float(Ctemp), float(Ftemp)]),
+
+		{fahrenheitOK, Ftemp, Ctemp} ->
+			io:format("~.2f F is equivalent to ~.2f C~n", [float(Ftemp), float(Ctemp)]),
+
 		test -> 
 			io:format("test message received by displayer"),
 			loop()
