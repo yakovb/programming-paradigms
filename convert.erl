851bf5e71ce7b63068868dd89c639de2534be4fe
yakovb <yakov.boglev@gmail.com>
2015-04-19 17:15:16 +0100

    convert notifies if it got bad data, doesn't exit

--- convert.erl
+++ convert.erl
@@ -14,7 +14,7 @@ loop() ->
 			loop();
 
 		_ -> 
-			exit(convert, "got bad data to process"),
+			io:format("got bad data to process"),
 			loop()
 			
 		end.
@@ -22,12 +22,12 @@ loop() ->
 
 
 c2f(Ctemp) -> 	if 
-					Ctemp < -273.15 -> {zero_error, c, Ctemp};
+					Ctemp < -273.15 -> {zero_error_C, Ctemp};
 					true -> {celsiusOK, Ctemp, Ctemp * (9/5) + 32}
 				end.
 
 
 f2c(Ftemp) -> 	if 
-					Ftemp < -523.67 -> {zero_error, f, Ftemp};
+					Ftemp < -523.67 -> {zero_error_F, Ftemp};
 					true -> {fahrenheitOK, Ftemp, (Ftemp - 32) * (5/9)}
 				end.
