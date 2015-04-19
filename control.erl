7f9718db1928dd4fbe5c51a9bd47622ecb6d2653
yakovb <yakov.boglev@gmail.com>
2015-04-19 16:16:46 +0100

    update TODOs

--- control.erl
+++ control.erl
@@ -5,6 +5,7 @@
 %TODO: create temp list for testing including temps in/out of range for F and C
 %TODO: create multiple converters and displays if possible
 %TODO: have a system shutdown option
+%TODO note if converter dies and restart it
 
 loop() ->
 	receive
