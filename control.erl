f2576136ca5b20ac25370861bd896ce034793a05
yakovb <yakov.boglev@gmail.com>
2015-04-19 23:36:31 +0100

    Testing data input but control flow is off. Need to stage the testing of each list so that there's enough time to do it all. Process is now shutting down before testing anything and not sure why.

--- control.erl
+++ control.erl
@@ -25,6 +25,36 @@ loop() ->
 
 
 start() ->
-	self() ! kick_off,
-	CListGood = [23, -100, 896, 46, 99751],
-	[self() ! {convert_C, C} || C <- CListGood].
+	Me = spawn(fun control:loop/0),
+	Me ! kick_off,
+	io:format("~nTesting Celsius conversion and display...~n"),
+	celsius_good(Me), celsius_too_cold(Me), celsius_bad_data(Me),
+	io:format("~nTesting Fahrenheit conversion and display...~n"),
+	fahrenheit_good(Me), fahrenheit_too_cold(Me), fahrenheit_bad_data(Me),
+	Me ! shutdown.
+
+celsius_good(PID) ->
+	io:format("~nThe next 5 Celsius temps should all convert correctly: ~n"),
+	rollingSend({PID, convert_C,  [0, -100, 896, 46, 99751]}).
+
+celsius_too_cold(PID) ->
+	io:format("~nThe next 2 Celsius temps should all fail because they're below absolute zero: ~n"),
+	rollingSend({PID, convert_C,  [-1000, -273.16]}).
+
+celsius_bad_data(PID) ->
+	io:format("~nThe next 3 Celsius conversions should fail as they're not numeric data: ~n"),
+	rollingSend({PID, convert_C,  [test, "string", [atom, list]]}).
+
+fahrenheit_good(PID) ->
+	io:format("~nThe next 5 Fahrenheit temps should all convert correctly: ~n"),
+	rollingSend({PID, convert_F,  [0, -100, 896, 46, 99751]}).
+
+fahrenheit_too_cold(PID) ->
+	io:format("~nThe next 2 Fahrenheit temps should all fail because they're below absolute zero: ~n"),
+	rollingSend({PID, convert_F,  [-1000, -523.68]}).
+
+fahrenheit_bad_data(PID) ->
+	io:format("~nThe next 3 Fahrenheit conversions should fail as they're not numeric data: ~n"),
+	rollingSend({PID, convert_F,  [test, "string", [atom, list]]}).
+
+rollingSend({PID, Conv, Nums}) -> lists:foreach(fun(X) -> PID ! {Conv, X} end, Nums).
