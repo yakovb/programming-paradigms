a8a6139d7053a139b6039e93a71c9a4a4ae8baa5
yakovb <yakov.boglev@gmail.com>
2015-04-19 14:56:58 +0100

    C to F function put into receive loop

--- convert.erl
+++ convert.erl
@@ -1,7 +1,6 @@
 -module(convert).
 -export([loop/0]).
 
-%TODO: convert from F and C as per formula
 %TODO: error check for temps below absolute zero (is there a max temp??)
 %TODO: send results to display
 
@@ -9,6 +8,10 @@ loop() ->
 	receive
 		{"ConvertToCelsius", Ftemp} ->
 			io:format("~.2f F is equivalent to ~.2f C~n", [float(Ftemp), f2c(Ftemp)]), %TODO debug text, remove
+			loop();
+
+		{"ConvertToFahrenheit", Ctemp} ->
+			c2f(Ctemp),
 			loop();
 
 		test -> 
