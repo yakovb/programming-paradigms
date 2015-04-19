83f1d8389e15cad145c7d9b4616f3a6aab6c05d0
yakovb <yakov.boglev@gmail.com>
2015-04-19 17:18:02 +0100

    control can start new actors Will not let convert die if it gets bad data. It will simply notify and keep going

--- control.erl
+++ control.erl
@@ -1,25 +1,17 @@
 -module(control).
 -export([loop/0]).
 
-%TODO: spawn/kill/restart converters and displays
+%TODO: kill converters and displays
 %TODO: create temp list for testing including temps in/out of range for F and C
-%TODO: create multiple converters and displays if possible
 %TODO: have a system shutdown option
-%TODO note if converter dies and restart it
 
 loop() ->
-	process_flag(trap_exit, true),
 	receive
-		new_converter ->
-			io:format("Starting a new converter actor.~n"),
+		kick_off ->
+			io:format("Starting a new converter actir and display actor.~n"),
 			register(converter, spawn_link(fun convert:loop/0)),
+			register(display, spawn_link(fun display:loop/0)),
 			loop();
 
-
-		{'EXIT', DeadConverter, Reason} ->
-			io:format("Converter ~p died with reason:  ~p.", [DeadConverter, Reason]),
-			io:format("Launching another converter...~n"),
-			self ! new_converter,
-			loop()
 
 		end.
