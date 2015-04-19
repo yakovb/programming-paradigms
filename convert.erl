761c5542dcbc336339d0dc6b244fb7ee620fe1bd
yakovb <yakov.boglev@gmail.com>
2015-04-19 15:26:33 +0100

    Test for absolute zero temp

--- convert.erl
+++ convert.erl
@@ -20,5 +20,21 @@ loop() ->
 			
 		end.
 
+c2f(Ctemp) -> if
+				Ctemp =< 273.15 -> 
+					{error, "Requested temp is below absolute zero"};
+				true ->
+					Ctemp * (9/5) + 32.
+
 f2c(Ftemp) -> (Ftemp - 32) * (5/9).
-c2f(Ctemp) -> Ctemp * (9/5) + 32.
+
+
+% checks if the temperature is below absolute zero
+zeroTest(T) -> case T of
+	{c, C} -> 	if
+					Ctemp =< 273.15 -> false;
+					true -> true.
+				end;
+
+	{f, F} -> zeroTest(f2c(F))
+end.
