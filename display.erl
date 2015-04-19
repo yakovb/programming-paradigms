a3cf9eaded1435534ae58a23256c557ca2f34a09
yakovb <yakov.boglev@gmail.com>
2015-04-19 16:48:11 +0100

    corrected error display code (conv arg to float first)

--- display.erl
+++ display.erl
@@ -13,12 +13,12 @@ loop() ->
 			io:format("~.2f F is equivalent to ~.2f C~n", [float(Ftemp), float(Ctemp)]),
 			loop();
 
-		{zero_error_C, X} ->
-			io:format("Oops! ~.2f C is below absolute zero of -273.15 C so conversion is impossible~n", [X]),
+		{zero_error_C, C} ->
+			io:format("Oops! ~.2f C is below absolute zero of -273.15 C so conversion is impossible~n", [float(C)]),
 			loop();
 
-		{zero_error_F, X} ->
-			io:format("Oops! ~.2f F is below absolute zero of -523.67 F so conversion is impossible~n", [X]),
+		{zero_error_F, F} ->
+			io:format("Oops! ~.2f F is below absolute zero of -523.67 F so conversion is impossible~n", [float(F)]),
 			loop()
 			
 		end.
