944c156d1c787f0468e921d81014f1f1fe09e3b1
yakovb <yakov.boglev@gmail.com>
2015-04-21 22:30:37 +0100

    formatting text

--- control.erl
+++ control.erl
@@ -44,27 +44,27 @@ tester() ->
 	Control ! shutdown.
 
 celsius_good(PID) ->
-	io:format("The next 5 Celsius temps should all convert correctly: ~n"),
+	io:format("~nThe next 5 Celsius temps should all convert correctly: ~n"),
 	rollingSend({PID, convert_C,  [0, -100, 896, 46, 99751]}).
 
 celsius_too_cold(PID) ->
-	io:format("The next 2 Celsius temps should all fail because they're below absolute zero: ~n"),
+	io:format("~nThe next 2 Celsius temps should all fail because they're below absolute zero: ~n"),
 	rollingSend({PID, convert_C,  [-1000, -273.16]}).
 
 celsius_bad_data(PID) ->
-	io:format("The next 3 Celsius conversions should fail as they're not numeric data: ~n"),
+	io:format("~nThe next 3 Celsius conversions should fail as they're not numeric data: ~n"),
 	rollingSend({PID, convert_C,  [test, "string", [atom, list]]}).
 
 fahrenheit_good(PID) ->
-	io:format("The next 5 Fahrenheit temps should all convert correctly: ~n"),
+	io:format("~nThe next 5 Fahrenheit temps should all convert correctly: ~n"),
 	rollingSend({PID, convert_F,  [0, -100, 896, 46, 99751]}).
 
 fahrenheit_too_cold(PID) ->
-	io:format("The next 2 Fahrenheit temps should all fail because they're below absolute zero: ~n"),
+	io:format("~nThe next 2 Fahrenheit temps should all fail because they're below absolute zero: ~n"),
 	rollingSend({PID, convert_F,  [-1000, -523.68]}).
 
 fahrenheit_bad_data(PID) ->
-	io:format("The next 3 Fahrenheit conversions should fail as they're not numeric data: ~n"),
+	io:format("~nThe next 3 Fahrenheit conversions should fail as they're not numeric data: ~n"),
 	rollingSend({PID, convert_F,  [test, "string", [atom, list]]}).
 
 rollingSend({PID, Conv, Nums}) -> 
